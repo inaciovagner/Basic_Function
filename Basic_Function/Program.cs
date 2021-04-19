4471ee0f79cc5d296b6873d95b40bd51ef426cba
Vagner de Souza <inaciovagner1@gmail.com>
2021-04-19 16:19:26 +0100

    Add project files.

---
+++ Basic_Function/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Function
+{
+    class Program
+    {
+        static void Write()
+        {
+            Console.WriteLine("Text output from function");
+        }
+        static void Main(string[] args)
+        {
+            Write();
+            Console.ReadKey();
+        }
+    }
+}
